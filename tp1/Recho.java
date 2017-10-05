50cc6c719c29315203b36b6f7b2f6e1f1fc10ee8
NataliiaDeshko <32390347+NataliiaDeshko@users.noreply.github.com>
2017-10-05 19:05:29 +0200

    comments added

--- tp1/Recho.java
+++ tp1/Recho.java
@@ -2,13 +2,16 @@ public class Recho {
 
     public static void main(String[] args) {
 
+		// check whether there are any arguments entered
 		if (args.length>0){
 			
-        for (int i=args.length-1;i>0;i--)
-            System.out.print(args[i]+" ");
-
+			// going through the entered strings from the last one to the second
+			for (int i=args.length-1;i>0;i--)
+				// displaying them in a reversed order
+				System.out.print(args[i]+" ");
         
-            System.out.print(args[0]);
+			// displaying the first string which wasn't shown in the loop
+			System.out.print(args[0]);
 		}
     }
 }
